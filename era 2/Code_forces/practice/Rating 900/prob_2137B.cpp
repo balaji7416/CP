c8bb0d6e474ace11b62e15c507a64ad6bb8a0ec3
balaji7416 <balaji.nani12345@gmail.com>
2025-09-23 14:28:27 +0530

    23-09-2025 CP problem-2 done

---
+++ era 2/Code_forces/practice/Rating 900/prob_2137B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        int size = arr.size();
+        for (auto i : arr)
+        {
+            cout << size + 1 - i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
